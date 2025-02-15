73639d1b981098b92a3e53b03e4615a02a92e5e4
Jonas <ermert.jonas@yahoo.de>
2025-02-15 10:26:45 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BarcodeApp());
+}
+
+class BarcodeApp extends StatelessWidget {
+  const BarcodeApp({super.key});
+
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Barcode App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+        useMaterial3: true,
+      ),
+    home: const HomeScreen(),
+  }
+}
